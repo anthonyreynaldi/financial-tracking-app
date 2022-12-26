683e83e03d6ac4bf5d1c14185dffd69bc3d38411
kellyflorences <kellyflorence12@gmail.com>
2022-12-26 19:02:51 +0700

    login register

--- app/src/main/java/com/example/proyek_android/MainActivity.kt
+++ app/src/main/java/com/example/proyek_android/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.proyek_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         println("Test repo")
         println("Test repo2")
         println("Test repo3")
+
+
+        val et_email = findViewById<EditText>(R.id.et_email)
+        val et_password = findViewById<EditText>(R.id.et_password)
+        val tv_register = findViewById<TextView>(R.id.tv_register)
+
+        tv_register.setOnClickListener{
+            startActivity(Intent(this, register::class.java))
+        }
     }
 }
