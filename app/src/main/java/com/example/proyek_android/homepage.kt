8a17b6e125e794ebe98aca3fd3f0736125711adf
kellyflorences <kellyflorence12@gmail.com>
2022-12-29 10:24:17 +0700

    login signup logout

--- app/src/main/java/com/example/proyek_android/homepage.kt
+++ app/src/main/java/com/example/proyek_android/homepage.kt
@@ -1,10 +1,12 @@
 package com.example.proyek_android
 
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,41 @@ class homepage : AppCompatActivity() {
         var btn_history = findViewById<ImageView>(R.id.btn_history)
         var btn_kategori = findViewById<ImageView>(R.id.btn_tambahkategori)
         var btn_sumber = findViewById<ImageView>(R.id.btn_tambahkategori)
+        var btn_logout = findViewById<ImageView>(R.id.btn_logout)
+        var btn_profile = findViewById<ImageView>(R.id.btn_profile)
         var tv_user = findViewById<TextView>(R.id.tv_user)
+        var view = findViewById<View>(R.id.homepage_view)
+
+        var sp = getSharedPreferences("user_login", MODE_PRIVATE)
+        val isisp = sp.getString("username", null)
+        if(isisp == null){
+            startActivity(Intent(this, MainActivity::class.java))
+        }else{
+            tv_user.setText(isisp)
+        }
+
+        btn_logout.setVisibility(View.GONE)
+
+        var dropdown = false
+        btn_profile.setOnClickListener{
+            if (!dropdown){
+                btn_logout.setVisibility(View.VISIBLE)
+                dropdown = true
+            }else{
+                btn_logout.setVisibility(View.GONE)
+                dropdown = false
+            }
+        }
+
+        btn_logout.setOnClickListener{
+            sp = getSharedPreferences("user_login", MODE_PRIVATE)
+            val editor = sp.edit()
+            editor.putString("username", null)
+            editor.apply()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+    override fun onBackPressed() {
+        Log.d("hehe", "hehe")
     }
 }
--- app/src/main/java/com/example/proyek_android/register.kt
+++ app/src/main/java/com/example/proyek_android/register.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 
 class register : AppCompatActivity() {
@@ -30,21 +32,86 @@ class register : AppCompatActivity() {
         et_confpass = findViewById<EditText>(R.id.et_confpass)
         var btn_register = findViewById<Button>(R.id.btn_register)
         var tv_login = findViewById<TextView>(R.id.tv_login)
+        val contextView = findViewById<View>(R.id.view)
+
+        var sp = getSharedPreferences("user_login", MODE_PRIVATE)
+        val isisp = sp.getString("username", null)
+        if(isisp != null){
+            startActivity(Intent(this, homepage::class.java))
+        }
 
         btn_register.setOnClickListener{
-            if(et_pass.text.toString() == et_confpass.text.toString()){
-                TambahData(db, et_name.text.toString(), et_email.text.toString(), et_phone.text.toString(), et_pass.text.toString())
+            //READ DATA
+            db.collection("user").get()
+                .addOnSuccessListener { result ->
+                    dataUser.clear()
+                    for(document in result){
+                        val namaBaru = user(document.data.get("name").toString(),
+                            document.data.get("email").toString(),
+                            document.data.get("phone").toString(),
+                            document.data.get("pass").toString())
+                        dataUser.add(namaBaru)
+                    }
+                }.addOnFailureListener{
+                    Log.d("Firebase", it.message.toString())
+                }
+
+            if(et_name.text.toString() != "" && et_email.text.toString() != "" &&
+                et_phone.text.toString() != "" && et_pass.text.toString() != ""){
+                if(et_pass.text.toString() == et_confpass.text.toString()){
+                    var bool = false
+                    for(i in dataUser){
+                        if(et_email.text.toString() == i.Email){
+                            bool = true
+                            break
+                        }
+                    }
+                    if(!bool){
+                        TambahData(db, et_name.text.toString(), et_email.text.toString(), et_phone.text.toString(), et_pass.text.toString())
+                        sp = getSharedPreferences("user_login", MODE_PRIVATE)
+                        val editor = sp.edit()
+                        editor.putString("username", et_name.text.toString())
+                        editor.apply()
+                        startActivity(Intent(this, homepage::class.java))
+                    }else{
+                        //email is taken
+                        Toast.makeText(contextView.context,"Email sudah terdaftar",Toast.LENGTH_LONG).show()
+                    }
+                }else{
+                    //pass and conf pass doesn't match
+                    Toast.makeText(contextView.context,"Password dan Confirm Password tidak sama",Toast.LENGTH_LONG).show()
+                }
+            }else{
+                //ada yang belum diisi
+                Toast.makeText(contextView.context,"Mohon isi semua field",Toast.LENGTH_LONG).show()
+
             }
+
         }
         tv_login.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
+    fun readData(db: FirebaseFirestore){
+        db.collection("user").get()
+            .addOnSuccessListener { result ->
+                dataUser.clear()
+                for(document in result){
+                    val namaBaru = user(document.data.get("name").toString(),
+                        document.data.get("email").toString(),
+                        document.data.get("phone").toString(),
+                        document.data.get("pass").toString())
+                    dataUser.add(namaBaru)
+                    Log.d("hehe1", dataUser.size.toString())
+                }
+            }.addOnFailureListener{
+                Log.d("Firebase", it.message.toString())
+            }
+    }
+
     fun TambahData(db: FirebaseFirestore, Name: String, Email: String, Phone: String, Pass: String){
         val userBaru = user(Name, Email, Phone, Pass)
-        readData(db)
-//        for(int )
         db.collection("user").document(Email)
             .set(userBaru)
             .addOnSuccessListener {
@@ -59,21 +126,7 @@ class register : AppCompatActivity() {
             }
     }
 
-    fun readData(db: FirebaseFirestore){
-        db.collection("user").get()
-            .addOnSuccessListener { result ->
-                dataUser.clear()
-                for(document in result){
-                    val namaBaru = user(document.data.get("Name").toString(),
-                        document.data.get("Email").toString(),
-                        document.data.get("Phone").toString(),
-                        document.data.get("Pass").toString())
-                    dataUser.add(namaBaru)
-                }
-            }.addOnFailureListener{
-                Log.d("Firebase", it.message.toString())
-            }
-    }
+
 
 //    fun getData(db: FirebaseFirestore, Email: String){
 //        db.collection("user").get()
